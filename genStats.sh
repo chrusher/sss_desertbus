786537745c30b9ed0f9c87031a9d82db34309dc6
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2020-02-17 16:04:53 +0000

    Bump PHP memory up to 1GB

--- genStats.sh
+++ genStats.sh
@@ -20,7 +20,7 @@ echo "### Splitting single .weechatlog into daily logs..."
 python2 $conf_dir/splitLogs.py -f $source_log -p $logs_dir/newserver.desertbus. -s .weechatlog -c
 
 echo "### Importing daily logs and generating stats page..."
-php -d memory_limit=768M $sss_dir/sss.php -c $conf_dir/desertbus.conf -i $logs_dir -o $website_dir/index.html &>> $conf_dir/sss.log
+php -d memory_limit=1024M $sss_dir/sss.php -c $conf_dir/desertbus.conf -i $logs_dir -o $website_dir/index.html &>> $conf_dir/sss.log
 
 rm "$conf_dir/stats-gen.lock"
 
